0291fa3eb36f000fed393c70fb16c14d8148f6d4
forchannot <yy320206@gmail.com>
2023-04-23 12:10:42 +0800

    :poop: add try for some http

--- nonebot_plugin_rename/card/one_word.py
+++ nonebot_plugin_rename/card/one_word.py
@@ -10,6 +10,9 @@ async def get_one_speak() -> Optional[str]:
     async with httpx.AsyncClient(follow_redirects=True) as client:
         try:
             res = await client.get("https://v1.hitokoto.cn/")
+            if res.status_code != 200:
+                logger.warning(f"获取一言失败: {res.status_code}")
+                return None
             data = res.json()["hitokoto"]
             return data[:16]
         except Exception as e:
--- nonebot_plugin_rename/utils/download.py
+++ nonebot_plugin_rename/utils/download.py
@@ -6,10 +6,13 @@ from nonebot import logger
 
 async def download_file(url: str, file_path: Path) -> None:
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                with file_path.open(mode='wb') as f:
-                    f.write(await resp.read())
-                logger.info("字体文件下载成功")
-            else:
-                logger.error(f'下载失败，状态码：{resp.status}')
+        try:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    with file_path.open(mode='wb') as f:
+                        f.write(await resp.read())
+                    logger.info("字体文件下载成功")
+                else:
+                    logger.error(f'下载失败，状态码：{resp.status}')
+        except Exception as e:
+            logger.error(f'下载失败，错误信息：{e}')
