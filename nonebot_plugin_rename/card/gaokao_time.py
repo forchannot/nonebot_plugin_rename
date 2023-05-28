ef1812437f336b7b32584bf9762d5b6866880a73
zmy15 <109537161+zmy15@users.noreply.github.com>
2023-05-28 15:07:04 +0800

    ✨添加高考年份

--- nonebot_plugin_rename/card/gaokao_time.py
+++ nonebot_plugin_rename/card/gaokao_time.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 def gk() -> str:
     # 基准时间
+    year = 2023
     baseTime = (
         datetime.strptime("2023-6-7 9:00:00", "%Y-%m-%d %H:%M:%S").timestamp() * 1000
     )
@@ -12,8 +13,9 @@ def gk() -> str:
     # 获取持续时间
     duringTime = baseTime - nowTime
     while duringTime <= 0:
-        # 时间+365天
+        # 时间+365天 年份+1年
         duringTime += 365 * 24 * 60 * 60 * 1000
+        year += 1
     # 获取天数并取整
     days = int(duringTime / (24 * 3600 * 1000))
     leave1 = duringTime % (24 * 3600 * 1000)
@@ -22,4 +24,4 @@ def gk() -> str:
     leave2 = leave1 % (3600 * 1000)
     # 获取分钟数并取整
     minutes = int(leave2 / (60 * 1000))
-    return f"离高考还有{days}天{hours}小时{minutes}分钟"
+    return f"离{year}年高考还有{days}天{hours}小时{minutes}分钟"
