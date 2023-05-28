292d0d8e6a24d7555590d6baa696a21a41afceca
forchannot <yy320206@gmail.com>
2023-05-28 17:13:22 +0800

    :art: 更改注解

--- nonebot_plugin_rename/config.py
+++ nonebot_plugin_rename/config.py
@@ -1,11 +1,12 @@
 # Description: 插件的配置文件
 from pydantic import BaseModel, Extra
+from typing import Optional
 
 
 class Config(BaseModel, extra=Extra.ignore):
     set_group_card_hour: int = 0
     set_group_card_minute: int = 30
     use_nickname_front: bool = True
-    self_name: str = None
+    self_name: Optional[str] = None
     is_one_bot_set_all_group_card: bool = False
     is_show_aps_info_log: bool = True
