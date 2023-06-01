25d6874de1068243163ba0a1b1745384d50e43b8
zmy15 <109537161+zmy15@users.noreply.github.com>
2023-06-01 22:02:33 +0800

    ✨修复计算bug

--- nonebot_plugin_rename/card/gaokao_time.py
+++ nonebot_plugin_rename/card/gaokao_time.py
@@ -1,13 +1,19 @@
 import datetime
 
 
-def gk() -> str:
+def gk():
     now = datetime.datetime.now()
     current_year = now.year
 
     # 设置目标年份的高考日期，每年的6月7日9点到6月8日17点
     gk_start = datetime.datetime(current_year, 6, 7, 9)
     gk_end = datetime.datetime(current_year, 6, 8, 17)
+
+    # 判断是否已经超过当年的高考日期
+    if now > gk_end:
+        current_year += 1
+        gk_start = datetime.datetime(current_year, 6, 7, 9)
+        gk_end = datetime.datetime(current_year, 6, 8, 17)
 
     if gk_start <= now <= gk_end:
         return f"{current_year}年高考正在进行中"
