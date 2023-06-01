f342d22fbf4a388e6cef17e2639adcd013f3d0c6
forchannot <yy320206@gmail.com>
2023-06-01 16:40:45 +0800

    :art: update pluginmetadata

--- nonebot_plugin_rename/__init__.py
+++ nonebot_plugin_rename/__init__.py
@@ -7,6 +7,8 @@ __plugin_meta__ = PluginMetadata(
     name="nonebot_plugin_rename",
     description="用于更改qq机器人的群名片，内置多种有趣名片",
     usage="查看本仓库readme",
+    supported_adapters={"~onebot.v11"},
+    homepage="https://github.com/forchannot/nonebot_plugin_rename",
     extra={
         "author": "forchannot",
         "version": __version__,
