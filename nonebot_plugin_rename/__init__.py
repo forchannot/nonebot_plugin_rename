78ef4e05e7b13299581fd54ef4a128271e8e2952
forchannot <yy320206@gmail.com>
2023-06-01 16:42:06 +0800

    :bookmark: 1.3.6

--- nonebot_plugin_rename/__init__.py
+++ nonebot_plugin_rename/__init__.py
@@ -2,12 +2,13 @@ from nonebot.plugin import PluginMetadata
 
 from .main import *  # noqa
 
-__version__ = "1.3.5"
+__version__ = "1.3.6"
 __plugin_meta__ = PluginMetadata(
     name="nonebot_plugin_rename",
     description="用于更改qq机器人的群名片，内置多种有趣名片",
     usage="查看本仓库readme",
     supported_adapters={"~onebot.v11"},
+    type="application",
     homepage="https://github.com/forchannot/nonebot_plugin_rename",
     extra={
         "author": "forchannot",
