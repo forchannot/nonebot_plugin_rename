b29dbe8d0c582dff77938c3215464ee109dadbfc
forchannot <yy320206@gmail.com>
2023-06-21 13:55:47 +0800

    :art: 更换热搜api

--- nonebot_plugin_rename/card/hot_search.py
+++ nonebot_plugin_rename/card/hot_search.py
@@ -6,25 +6,33 @@ from typing import Optional
 import httpx
 from nonebot import logger
 
+# URL = {
+#     "1": "https://tenapi.cn/v2/bilihot/",  # B站
+#     "2": "https://tenapi.cn/v2/weibohot",  # 微博
+#     "3": "https://tenapi.cn/v2/douyinhot",  # 抖音
+#     "4": "https://tenapi.cn/v2/baiduhot/",  # 百度
+#     "5": "https://tenapi.cn/v2/zhihuhot",  # 知乎
+#     "6": "https://tenapi.cn/v2/toutiaohot",  # 今日头条
+# }
 URL = {
-    "1": "https://tenapi.cn/v2/bilihot/",  # B站
-    "2": "https://tenapi.cn/v2/weibohot",  # 微博
-    "3": "https://tenapi.cn/v2/douyinhot",  # 抖音
-    "4": "https://tenapi.cn/v2/baiduhot/",  # 百度
-    "5": "https://tenapi.cn/v2/zhihuhot",  # 知乎
-    "6": "https://tenapi.cn/v2/toutiaohot",  # 今日头条
+    "1": "bilibili",  # B站
+    "2": "weibo",  # 微博
+    "3": "newsqq",  # 腾讯新闻
+    "4": "baidu",  # 百度
+    "5": "zhihu",  # 知乎
+    "6": "toutiao",  # 今日头条
 }
 
 
 async def hot(num: int) -> Optional[str]:
     async with httpx.AsyncClient(follow_redirects=True) as client:
         try:
-            res = await client.get(URL[str(num)])
+            res = await client.get(f"https://hot.zhenxun.buzz/{URL[str(num)]}")
             if res.status_code != 200:
                 return "热搜api失效"
             data = res.json()["data"]
-            result = random.choice(data)["name"]
-            return result[:16]
+            result = random.choice(data)["title"]
+            return result
         except Exception as e:
             logger.warning(f"获取热搜失败: {e}")
             return None
